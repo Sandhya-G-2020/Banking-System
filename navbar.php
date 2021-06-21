8e409354ee699492b73a5418e9cbf4850efa4615
Sandhya-G-2020 <71721402+Sandhya-G-2020@users.noreply.github.com>
2021-06-21 14:20:56 +0530

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -10,7 +10,9 @@
               <li class="nav-item">
                 <a class="nav-link" href="index.php">Home</a>
               </li>
-              
+              <li class="nav-item">
+                <a class="nav-link" href="createuser.php">Create User</a>
+              </li>
               <li class="nav-item">
                 <a class="nav-link" href="transfer.php">Transfer Money</a>
               </li>
